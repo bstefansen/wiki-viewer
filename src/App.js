dd0b6e8bc7708f042fc3d404de24f9dcf37594f8
bstefansen <54404576+bstefansen@users.noreply.github.com>
2020-05-24 16:34:51 -0400

    created search bar and basic functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      submit: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    // create method to fetch API with submit data
+    
+    /*this.setState({
+      submit: this.state.input,
+      input: ""
+    });
+    */
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Enter a title to search</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+          type="text"
+          placeholder="google" 
+          value={this.state.input}
+          onChange={this.handleChange} />
+          <button type='submit'>Submit</button>
+        </form>
+        <h2>{this.state.submit}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
